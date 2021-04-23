038cb364b60b706035a56aaa81940d679ac258bf
Dean Richer <dpbricher@gmail.com>
2021-04-23 11:06:34 +0100

    Refactored flatten.js

--- flatten.js
+++ flatten.js
@@ -1,43 +1,14 @@
 function flatten(obj) {
-	if (obj.length) {
-		var flatArray = [];
-
-		if (obj.length === 1) {
-			return obj[0];
-		}
-
-		for (var i = 0; i < obj.length; i++) {
-			var flatItem = flatten(obj[i]);
-
-			if (flatItem.length) {
-				for (var j = 0; j < flatItem.length; j++) {
-					flatArray.push(flatItem[j]);
-				}
-			} else {
-				flatArray.push(flatItem);
-			}
-		}
-
-		return flatArray;
+	if (Array.isArray(obj)) {
+		return obj.reduce((flatArray, item) => {
+			let flatItem = flatten(item)
+			return flatArray.concat(flatItem)
+		}, [])
 	}
 
 	if (typeof obj === 'object') {
-		var asArray = [];
-
-		for (var key in obj) {
-			var flatItem = flatten(obj[key]);
-
-			if (flatItem.length) {
-				for (var j = 0; j < flatItem.length; j++) {
-					asArray.push(flatItem[j]);
-				}
-			} else {
-				asArray.push(flatItem);
-			}
-		}
-
-		return asArray;
+		return flatten(Object.values(obj))
 	}
 
-	return obj;
+	return obj
 }
